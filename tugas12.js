d0a261d3aae1d53cd60087871f659481f69dcbf2
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-09-21 08:48:48 +0700

    Add files via upload

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+var dataBuah=["Pisang", "Jeruk", "Apel", "Mangga"]
+console.log (dataBuah)
+
+
+console.log("pop - menghapus data terakhir")
+dataBuah.pop()
+console.log (dataBuah)
+console.log("shift - menghapus data pertama")
+dataBuah.shift()
+console.log (dataBuah)
